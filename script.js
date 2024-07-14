600fcd2ad8f440f1e8b632dc3d78d6d24200e816
Fabian <goldenlava11@gmail.com>
2024-07-14 16:29:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function greetUser() {
-    const nameInput = document.getElementById('nameInput').value;
-    const greetingMessage = document.getElementById('greetingMessage');
-    greetingMessage.textContent = `Hello, ${nameInput}! Welcome to our site.`;
+function submitQuote() {
+    const quoteInput = document.getElementById('quoteInput').value;
+    const quoteDisplay = document.getElementById('quoteDisplay');
+    quoteDisplay.textContent = `Veteran's Quote: "${quoteInput}`;
 }
